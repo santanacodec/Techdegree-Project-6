d3d6361170a8d7d4db8ee24e0134b45c0b1b87ed
Edgar Santana <santanaprogrammer@gmail.com>
2020-04-10 17:53:00 -0400

    fixed event listener

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const startGame = document.querySelector('a.btn__reset');
 let missed = 0;
 
 // Removes the Wheel Of Success overlay upon clicking 'start game'.
-startGame.addEventListener('click', (e) => {
+startGame.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
