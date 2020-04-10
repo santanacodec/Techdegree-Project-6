a6e87755adf0e7e9b68fc089f932b389412b726b
Edgar Santana <santanaprogrammer@gmail.com>
2020-04-10 17:45:44 -0400

    finished start game button

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const keyboard = document.getElementById('qwerty');
 const phraseToGuess = document.getElementById('phrase');
 const overlay = document.getElementById('overlay');
-const startGame = document.getElementsByClassName('btn_reset');
+const startGame = document.querySelector('a.btn__reset');
 
 let missed = 0;
 
 startGame.addEventListener('click', (e) => {
-
+    overlay.style.display = 'none';
 });
