d4e4a46a538489a4fdd7d2516ca544461bed9798
Edgar Santana <santanaprogrammer@gmail.com>
2020-04-10 23:44:46 -0400

    added phrases array
    created phrases array and started getRandomPhraseAsArray function.

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const phraseToGuess = document.getElementById('phrase');
 const overlay = document.getElementById('overlay');
 const startGame = document.querySelector('a.btn__reset');
 
+var phrases = [
+    "Beat around the bush",
+    "When pigs fly",
+    "Speak of the devil",
+    "A piece of cake",
+    "No pain no gain"
+];
+
 let missed = 0;
 
 // Removes the Wheel Of Success overlay upon clicking 'start game'.
 startGame.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
+
+function getRandomPhraseAsArray(arr) {
+
+}
